e6e7b6d0b73f899b3f88b5abd41ed73a5b2aecde
alfonsogonzalez <alfonso8gonzalez@gmail.com>
2015-11-30 17:21:06 -0800

    incorporated prefix, suffix and word length. added some other stuff too

--- words_with_friends.py
+++ words_with_friends.py
@@ -7,30 +7,89 @@ from time import time as time
 
 def letters():
     _letters = input('Input letters you have: ')
-    return _letters    
+    return _letters
 
-def check_if_word(letters):
+def prefix():
+    prompt = input('Prefix? (y/n): ')
+    if prompt == 'y':
+        _prefix = input('Input prefix: ')
+        return _prefix
+    else:
+        return None
+
+def suffix():
+    prompt = input('Suffix? (y/n): ')
+    if prompt == 'y':
+        _suffix = input('Input suffix: ')
+        return _suffix
+    else:
+        return None
+
+def length():
+    prompt = input('Specific length? (y/n): ')
+    if prompt == 'y':
+        length = int(input('Length?: '))
+        return length
+    else:
+        return None
+    
+def check_if_word(letters, prefix=None, suffix=None, length=None):
     possibles = []
     count = 0
     d = enchant.Dict('en_US')
     time1 = time()
-    length = len(letters) + 1
-    for num in range(2, length):
+    length_check = len(letters) + 1
+    for num in range(2, length_check):
         permute = permutations(letters, num)
         for arrangement in permute:
-            string = ''.join(arrangement)
+            if prefix == None and suffix == None:
+                string = ''.join(arrangement)
+                
+            elif prefix != None and suffix == None:
+                string = prefix
+                string += ''.join(arrangement)
+
+            elif prefix == None and suffix != None:
+                string = ''.join(arrangement)
+                string += suffix
+
+            elif prefix != None and suffix != None:
+                string = prefix
+                string += ''.join(arrangement)
+                string += suffix
+                
+            if length == None:
+                pass
+            else:
+                string = string[0:length]
+                
             check = d.check(str(string))
             if check == True:
                 possibles.append(string)
             count += 1
+            if count == 1000:
+                print('Checked 1,000...')
+            if count == 13000:
+                print('Checked 13,000...')
+            if count == 100000:
+                print('Checked 100,000...')
+            if count == 500000:
+                print('Checked 500,000...')
     time2 = time()
     total_time = time2 - time1
     returns = {'possibles': possibles, 'count': count, 'time': total_time}
     return returns               
 
 def main(argv):
+    _prefix = prefix()
     _letters = letters()
-    _possibles = check_if_word(_letters)
+    _suffix = suffix()
+    _length = length()
+    
+    print('Checking all possibles combinations...')
+
+    _possibles = check_if_word(letters=_letters, prefix=_prefix, suffix=_suffix, length=_length)
+
     print('Possible words:')
     for word in _possibles['possibles']:
         print(word)
